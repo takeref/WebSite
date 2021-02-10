2ddc1bb26701e321dfab9db0ad8b634a9716a50e
takeref <takeref@gmail.com>
2021-02-09 23:16:08 -0600

    up02

--- index.py
+++ index.py
@@ -1,4 +1,14 @@
-a = "hello pycha Work"
-print(a)
-b = "test de test"
-print(b)
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route('/')
+def home():
+    return 'Hola Tk'
+
+app.route('/about')
+def about():
+    return 'Acerk de'
+
+if __name__ == '__main__':
+    app.run()
